876bf0549e8f9f094475e017752af80cccf5f4fe
Muhammad Nasrullah <nasrullahcoder@gmail.com>
2025-09-21 17:19:11 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,3 @@ main() {
     Quiz()
   );
 }
-
-// Alhumdulillah.
-
-// Make this More Clean aritecture and short code and post on Github.
-
-
-/*
-My Tasks 
-
-Learn CLI
-Learn Dart 
-Learn Flutter 
-
- Maximilian schwarzmuller
- Angela YU
- Vandad
- Rivan Rivant.
-
- ok let's Start.
-
-*/
